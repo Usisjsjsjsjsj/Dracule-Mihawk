58dabe8b0d0ee5b583ff0dc81c79880ded359edd
Usisjsjsjsjsj <102508520+Usisjsjsjsjsj@users.noreply.github.com>
2022-07-19 15:28:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,11 +12,11 @@ const chalk = require('chalk')
 global.zenzkey = 'Your_Apikey' //Register Atau Beli Di zenzapis.xyz
 
 //===> SESUAIKAN DENGAN KEBUTUHAN MU
-global.ownerNumber = ['6285609233482','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
-global.Rowner = '6285609233482' //Owner Utama
-global.ownerName = 'Nzrl Afndi' //Ganti Jadi Namamu
-global.packname = 'Dracule Mihawk' //Ganti Jadi Nama Botmu
-global.author = 'By NzrlAfndi' //Ganti Jadi Namamu
+global.ownerNumber = ['6282235716434','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
+global.Rowner = '6282235716434' //Owner Utama
+global.ownerName = 'dimas' //Ganti Jadi Namamu
+global.packname = 'dimas jb' //Ganti Jadi Nama Botmu
+global.author = 'samid' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi' //Saran Gausah Diganti Lagi
 global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
